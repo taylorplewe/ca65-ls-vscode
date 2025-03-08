8ae243d79f5d99186316bb002390ca7308fc501e
Taylor Plewe <tplewe@outlook.com>
2025-03-08 09:01:26 -0700

    still tryin

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,41 @@
 import * as vscode from 'vscode';
 
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('ca65-ls-vscode.helloWorld', () => {
-		vscode.window.showInformationMessage('i stg');
-	});
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind,
+} from 'vscode-languageclient/node';
 
-	context.subscriptions.push(disposable);
+let client: LanguageClient;
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log("here we go");
+	const serverOptions: ServerOptions = {
+		command: context.asAbsolutePath('asm6502-lsp'),
+		transport: TransportKind.stdio,
+	};
+
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: [
+			{ language: "ca65", scheme: "file" },
+			{ language: "ca65", scheme: "untitled" },
+		],
+	};
+
+	client = new LanguageClient(
+		'ca65-ls',
+		'ca65-ls',
+		serverOptions,
+		clientOptions,
+	);
+	client.start();
+	console.log('started!');
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
 }
